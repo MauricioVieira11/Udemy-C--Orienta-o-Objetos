88d29a85bae89ee7c02c0ab0500d3bd14e8f1dd7
Mauricio <mauriciovieira8@hotmail.com>
2021-10-15 15:02:06 -0300

    módulo 10

---
+++ Course10/Secao10/Entities/SavingsAccount.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Secao10HErancaPolimorfsmo.Entities
+{
+    class SavingsAccount : Account //A classe Savings Account herda tudo que a classe Account tem
+    {
+        public double InterestRate { get; set; }
+
+        
+        
+        public SavingsAccount()
+        {
+
+        }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate):base(number, holder, balance)//construtor com artumentos
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {              //método que atualiza o saldo da conta baseado na taxa de juros
+
+            Balance += Balance * InterestRate;
+        }
+    }
+}
